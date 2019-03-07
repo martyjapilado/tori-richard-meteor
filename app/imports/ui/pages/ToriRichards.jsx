76ed07cd8da2df1f456aeefdc2d22e159c9a3997
martyjapilado <martyjapilado@gmail.com>
2019-03-06 16:46:56 -1000

    More Complete

--- app/imports/ui/pages/ToriRichards.jsx
+++ app/imports/ui/pages/ToriRichards.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Container } from 'semantic-ui-react';import ToriRichardMain from '../components/ToriRichardMain';
+import { Container } from 'semantic-ui-react';  
+import ToriRichardMain from '../components/ToriRichardMain';
 
 export default class ToriRichard extends React.Component {
   render() {
